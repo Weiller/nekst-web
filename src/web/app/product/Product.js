2f1ea87da615c9d7f77fa9a5baf2cf1697ff49e8
Weiller Schepis <weillerschepis@gmail.com>
2019-07-10 23:55:34 -0300

    Configurando lint.

--- src/web/app/product/Product.js
+++ src/web/app/product/Product.js
@@ -1,11 +1,12 @@
 import * as React from 'react'
-import TextField from '@material-ui/core/TextField'
+import TextField from "@material-ui/core/TextField"
+import ProductList from './ProductList'
 
 const Product = () => {
 
     return (
         <form>
-            <TextField id='name' label='name' margin='normal'/>
+                                         <TextField id='name' label='name' margin='normal'/>
         </form>
     )
 
