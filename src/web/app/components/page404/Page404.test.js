f8a5f4634ca6da7ecbf5fcea9a19b3e9e1856246
Weiller Schepis <weillerschepis@gmail.com>
2019-07-10 22:59:52 -0300

    Corrigindo erro de build.

--- src/web/app/components/page404/Page404.test.js
+++ src/web/app/components/page404/Page404.test.js
@@ -1,14 +1,12 @@
 import React from 'react'
 import {shallow} from 'enzyme'
 import Page404 from "./Page404";
-import toJson from 'enzyme-to-json';
 
 
 describe('Teste do componente - Page 404', () => {
 
     it('test render', () => {
         const component = shallow(<Page404/>)
-        console.log(toJson(component))
-        expect(toJson(component)).toMatchSnapshot()
+        expect(component).toMatchSnapshot()
     });
 })
